7acc0eace932c0ab56fe1cb0377e75db9c200ce0
Ballistc-dot <pedrocdn2014@gmail.com>
2021-04-19 09:21:13 -0300

    Update Speedometter

--- html/index.js
+++ html/index.js
@@ -1,11 +1,47 @@
 $(function () {
+  function setProgressSpeed(value, element) {
+    var circle = document.querySelector(element);
+    var radius = circle.r.baseVal.value;
+    var circleference = radius * 2 * Math.PI;
+    var percent = value * 100 / 220;
+    var offset;
+    $('#speedometer_text_1').css({ fill: '#FFF' })
+    $('#speedometer_text_2').css({ fill: '#FFF' })
+    $('#speedometer_text_3').css({ fill: '#FFF' })
+    if (value.length >= 3) {
+      $('#speedometer_text_1').text(`${value[0]} `)
+      $('#speedometer_text_2').text(`${value[1]} `)
+      $('#speedometer_text_3').text(`${value[2]} `)
+    } else if (value.length >= 2) {
+      $('#speedometer_text_1').css({ fill: '#969494' }).text(0)
+      $('#speedometer_text_2').text(`${value[0]} `)
+      $('#speedometer_text_3').text(`${value[1]} `)
+    } else {
+      $('#speedometer_text_1').css({ fill: '#969494' }).text(0)
+      $('#speedometer_text_2').css({ fill: '#969494' }).text(0)
+      $('#speedometer_text_3').text(`${value[0]} `)
+
+    }
+    circle.style.strokeDasharray = `${circleference} ${circleference} `;
+    //circle.style.strokeDashoffset = `${circleference} `;
+
+    if (value <= 230) {
+      offset = + circleference + ((+ percent * 96) / 100) / 100 * circleference;
+
+      circle.style.strokeDashoffset = -offset;
+
+    } else {
+      offset = 690
+      circle.style.strokeDashoffset = -offset;
+    }
+  }
   window.addEventListener('message', (event) => {
 
     if (event.data.type == 'hud_update_info') {
-      $('#life_hud_hover').css("width", `${event.data.health}%`)
-      $('#bullet_proof_hud_hover').css("width", `${event.data.armor}%`)
+      $('#life_hud_hover').css("width", `${event.data.health}% `)
+      $('#bullet_proof_hud_hover').css("width", `${event.data.armor}% `)
     }
-    
+
     if (event.data.type == 'hud_update_info_top') {
       if (event.data.isInCar == 1) {
         $('.container').css('display', 'none')
@@ -25,12 +61,12 @@ $(function () {
         minutes = '0' + minutes
       }
       const time = hours + ':' + minutes
-      $('#street_name').text(`${event.data.street_name}`)
-      $('#hour').text(`${time}`)
+      $('#street_name').text(`${event.data.street_name} `)
+      $('#hour').text(`${time} `)
     }
     if (event.data.type == 'hud_update_info_speedometer') {
       const speed = Math.round(event.data.speed)
-      $('#speedometer').text(`${speed}`)
+      setProgressSpeed(`${speed}`, '.progress-speed');
     }
   })
 })
